06297874599a49bf80680fe9c64758365b13f29d
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-02-06 16:29:58 +0400

    feat: Meta data create a structure for the meta data of the raw packet

---
+++ include/meta_data.h
@@ -0,0 +1,27 @@
+#ifndef META_DATA_H_
+    #define META_DATA_H_
+    #include <stdint.h>
+
+    #define MAC_ADDR_SIZE 6
+
+    typedef struct __attribute__((packed)) meta_data_s {
+        uint8_t mac_dst[MAC_ADDR_SIZE];
+        uint8_t mac_src[MAC_ADDR_SIZE];
+        uint16_t eth_type;
+    } meta_data_t;
+
+    typedef enum eth_type_e {
+        IPV4 = 0x800,
+        IPV6 = 0x806,
+        ARP = 0x86DD
+    } eth_type_t;
+
+// void print_mac_addr(uint8_t mac_addr[MAC_ADDR_SIZE]);
+// const char *mac_addr_str(uint8_t mac_addr[MAC_ADDR_SIZE]);
+
+// eth_type_t get_eth_type(uint16_t eth_type);
+// void print_eth_type(uint16_t eth_type);
+// const char *eth_type_str(uint64_t eth_type);
+
+// const char *cast_type(uint8_t cast_type);
+#endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "raw_socket.h"
+#include "meta_data.h"
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
 bool running = true;
 
@@ -16,16 +19,27 @@ int main(void)
 {
     int raw_sockfd = RAW_SOCKET;
     raw_packet_t *packet = NULL;
+    meta_data_t meta_data = {0};
 
     while(running)
     {
         packet = raw_recvfrom(raw_sockfd, MAX_PACKET_SIZE, 0);
         if (packet)
         {
+            memset(&meta_data, 0, sizeof(meta_data_t));
             if (packet->data && packet->size > 1)
-                printf("packet: \n{\n\tsize: %lu\n\t%s\n}\n", packet->size, packet->data);
+            {
+                memcpy(&meta_data, packet->data, sizeof(meta_data_t));
+                for (uint8_t i = 0; i < MAC_ADDR_SIZE; i++)
+                    printf("%x:", meta_data.mac_dst[i]);
+                printf("\n");
+                for (uint8_t i = 0; i < MAC_ADDR_SIZE; i++)
+                    printf("%x:", meta_data.mac_src[i]);
+                printf("\n");
+                printf("%x\n", htons(meta_data.eth_type));
+            }
             delete_raw_packet(packet);
-        }   
+        }
         packet = NULL;
     }
     if (packet)
